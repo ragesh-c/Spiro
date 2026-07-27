4a2f449cecbe42001dd8b81072a3515fa931f4a0
projectkinda <project.mvp.bluepill@gmail.com>
2026-07-27 20:15:41 +0530

    Replace bare module import in js/sanity.js with CDN ESM & dynamic import fallback in case-study.html

--- js/sanity.js
+++ js/sanity.js
@@ -1,4 +1,4 @@
-import { createClient } from '@sanity/client'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@sanity/client@6.15.20/+esm'
 
 export const client = createClient({
   projectId: 'hdsri1qi',
